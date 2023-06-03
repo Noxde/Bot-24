635c9502e16136bd736d297f6e7f8b44fd8dbe7a
Noxde <mateo.tesorieri@hotmail.com>
2023-06-02 23:28:43 -0300

    Inicializa flag para saber si hay un juego en curso

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ const Client = require("./structures/Client.js");
 const client = new Client();
 require("dotenv").config();
 
+client.jugando = false;
+
 client.start(process.env.TOKEN);
 console.log(client.guilds);
